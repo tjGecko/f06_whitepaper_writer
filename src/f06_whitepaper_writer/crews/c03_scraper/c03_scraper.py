b18e05adb8edf533b9b291b6639e4c432c6cb32e
tj <tj.hoeft@gmail.com>
2025-01-24 04:42:02 -0500

    Crew 3 ready

---
+++ src/f06_whitepaper_writer/crews/c03_scraper/c03_scraper.py
@@ -0,0 +1,60 @@
+import hashlib
+
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from pydantic import BaseModel, Field
+
+
+from crewai_tools import FileWriterTool
+from crewai_tools import ScrapeWebsiteTool
+
+
+class ScrapeResult(BaseModel):
+    url: str = Field(..., description="URL that was scraped.")
+    scraped_content: str = Field(..., description="Text scraped from the web")
+
+    @property
+    def content_hash(self) -> str:
+        """Generate a hash string based on the model's content."""
+        hash_input = self.url
+        return hashlib.sha256(hash_input.encode('utf-8')).hexdigest()
+
+
+@CrewBase
+class C03Scraper:
+    """C03Scraper crew"""
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+
+    file_writer_tool = FileWriterTool()
+    scraper_tool = ScrapeWebsiteTool()
+
+    @agent
+    def page_scraper(self) -> Agent:
+        """Agent responsible for writing pages."""
+        return Agent(
+            config=self.agents_config['page_scraper'],
+            tools=[self.scraper_tool, self.file_writer_tool],
+            verbose=True
+        )
+
+    @task
+    def page_scraper_task(self) -> Task:
+        """Task to save research results as JSON files."""
+        return Task(
+            config=self.tasks_config['page_scraper_task'],
+            output_pydantic=ScrapeResult,
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the C02Crawler crew"""
+        # To learn how to add knowledge sources to your crew, check out the documentation:
+        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True
+        )
